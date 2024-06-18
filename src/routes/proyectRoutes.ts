5f724b8b60daa4a098cc8277fe31a305675e8142
trejo903 <paginawebesima@gmail.com>
2024-06-17 20:45:22 -0600

    First Commit

--- src/routes/proyectRoutes.ts
+++ src/routes/proyectRoutes.ts
@@ -80,4 +80,19 @@ router.post('/:projectId/tasks/:taskId/status',
     handleInputErrors,
     TaskController.updateStatus
 )
+
+router.get("/prueba", (req, res) => {
+    const htmlResponse = `
+      <html>
+        <head>
+          <title>NodeJs y Express en Vercel</title>
+        </head>
+        <body>
+          <h1>Soy un proyecto Back end en vercel</h1>
+        </body>
+      </html>
+    `;
+    res.send(htmlResponse);
+  });
+
 export default router
